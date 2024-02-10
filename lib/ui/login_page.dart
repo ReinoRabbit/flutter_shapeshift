29cdcabc3bc8ffe6e75ac01fccb96c3346094552
Reino Powell <rpowell@retrorabbit.co.za>
2024-02-10 15:55:07 +0200

    Added text fields

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shapeshift/components/logo_image.dart';
+import 'package:flutter_shapeshift/components/text_field.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,6 +10,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,8 +19,20 @@ class _LoginPageState extends State<LoginPage> {
         body: Container(
           color: Colors.white,
           child: Center(
-            child: Column(children: [logoWidget("lib/images/logo.png")])
-            
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(children: [
+                logoWidget("lib/images/logo.png"),
+                SizedBox(height: 5,),
+                Text("Login to Shape Shift",
+                  style: TextStyle(fontSize: 20),),
+                SizedBox(height: 50,),
+                textFieldWidget("Enter email here", Icons.person_outline, false, _emailTextController),
+                SizedBox(height: 40,),
+                textFieldWidget("Enter password here", Icons.lock_outline, true, _passwordTextController),
+                SizedBox(height: 40,),
+                ]),
+            )
           ),
         ),
       ),
