115a08344195d5817aa11d0a95a3fac666fbc83f
Reino-p <reinopowell61@gmail.com>
2024-02-10 17:43:13 +0200

    Added reuse button and stated other pages

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shapeshift/components/button_reuseable.dart';
 import 'package:flutter_shapeshift/components/logo_image.dart';
 import 'package:flutter_shapeshift/components/text_field.dart';
 
@@ -25,12 +26,13 @@ class _LoginPageState extends State<LoginPage> {
                 logoWidget("lib/images/logo.png"),
                 SizedBox(height: 5,),
                 Text("Login to Shape Shift",
-                  style: TextStyle(fontSize: 20),),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
                 SizedBox(height: 50,),
                 textFieldWidget("Enter email here", Icons.person_outline, false, _emailTextController),
                 SizedBox(height: 40,),
                 textFieldWidget("Enter password here", Icons.lock_outline, true, _passwordTextController),
                 SizedBox(height: 40,),
+                reusableButton(context, true, () {})
                 ]),
             )
           ),
